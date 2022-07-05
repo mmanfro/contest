87a2fcd0fb6f6dce1847c7f71c54d03e4171b176
Mauricio Manfro <6717975+mmanfro@users.noreply.github.com>
2022-07-04 21:15:28 -0300

    Update views.py

--- contest/views.py
+++ contest/views.py
@@ -7,23 +7,23 @@ def index(request):
 
 def handler500(request, exception=None):
     return render(
-        request, "imaginaria/error.html", context={"exception": exception}, status=500
+        request, "contest/error.html", context={"exception": exception}, status=500
     )
 
 
 def handler404(request, exception=None):
     return render(
-        request, "imaginaria/error.html", context={"exception": exception}, status=404
+        request, "contest/error.html", context={"exception": exception}, status=404
     )
 
 
 def handler403(request, exception=None):
     return render(
-        request, "imaginaria/error.html", context={"exception": exception}, status=403
+        request, "contest/error.html", context={"exception": exception}, status=403
     )
 
 
 def handler400(request, exception=None):
     return render(
-        request, "imaginaria/error.html", context={"exception": exception}, status=400
+        request, "contest/error.html", context={"exception": exception}, status=400
     )
